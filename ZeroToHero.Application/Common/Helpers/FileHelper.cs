47d7d8b68b4486cbacd7303a40f2cc112cf2ab77
Temurbey <ubaydullayev117@gmail.com>
2024-06-06 16:25:00 +0500

    FileService added

---
+++ ZeroToHero.Application/Common/Helpers/FileHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace ZeroToHero.Application.Common.Helpers;
+
+public class FileHelper
+{
+    public static string MakeFileName(string fileName)
+    {
+        string guid = Guid.NewGuid().ToString();
+        return "file_" + guid + fileName;
+    }
+
+    public static string MakeFileUrl(string partPath)
+        => "https://you-tube-web-app.herokuapp.com/" + partPath;
+}
