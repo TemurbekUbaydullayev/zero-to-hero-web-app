2bc8bf7f619be0fc9d076de527d0b8b6bcede0b4
Temurbey <ubaydullayev117@gmail.com>
2024-06-07 14:46:14 +0500

    Add FileService

---
+++ ZeroToHero.Application.Core/Core/IFileService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ZeroToHero.Application.Core.Core;
+
+public interface IFileService
+{
+    public string ResumeFolderName { get; }
+    Task<string> SaveFileAsync(IFormFile file);
+    Task<bool> DeleteFileAsync(string relativeResumePath);
+}
---
+++ ZeroToHero.Application.Core/Helper/FileHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace ZeroToHero.Application.Core.Helper;
+
+public static class FileHelper
+{
+    public static string MakeFileName(this string fileName)
+        => $"{fileName}-{Guid.NewGuid()}-cv";
+}
